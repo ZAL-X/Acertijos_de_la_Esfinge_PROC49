327a960b3ea04f27f87c6dcc12c8b3cf0b63e02a
ZAL-X <143039973+ZAL-X@users.noreply.github.com>
2024-01-29 00:18:24 -0600

    Add files via upload

---
+++ Acertijos_de_la_Esfinge_PROC49/README.md
@@ -0,0 +1,8 @@
+# Acertijos_de_la_Esfinge_PROC49
+Plantilla del alumno.  
+Búsqueda del tesoro. Etapa 1.  
+Modificación por parte del alumno de 2 comentarios.  
+  
+- Proyect Booster
+  
+### Texto en inglés: Treasure-hunt-template
---
+++ Acertijos_de_la_Esfinge_PROC49/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "SANDÍA";
+const accessCode2 = "FRANCIA";
+const accessCode3 = "AMAZON";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("Í A N A D S", 100,50)
+    fill("lightblue")
+    text("Pista: ¡Fruta roja y verde!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C R F A N A I", 700,150)
+    fill("lightblue")
+    text("Pista: ¡País europeo!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("M Z A N A O", 100,250)
+    fill("lightblue")
+    text("Pista: ¡Tienda virtual!", 100,270)
+
+}
